8d6eb791371021d5606d3ce7eb9877c9d41bc48e
gdm-1718-jantemme <jantemme@student.arteveldehs.be>
2019-03-19 00:53:16 +0100

    cdd/js worden niet gevonden. toppie

---
+++ sensehat_dashboard/assets/js/main.js
@@ -0,0 +1,1 @@
+document.getElementById("currentColor").innerHTML() = "<p> eyooo </p>"
--- sensehat_dashboard/pi/ambilight.py
+++ sensehat_dashboard/pi/ambilight.py
@@ -12,40 +12,52 @@ Docs: http://flask.pocoo.org/docs/1.0/
 # Import the libraries
 from flask import Flask, jsonify, render_template, request
 from sense_hat import SenseHat
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db
+from pyrebase import pyrebase
+import sys
+import os
 
-# Create an instance of flask
-app = Flask(__name__)
+config = {
+    "apiKey": "AIzaSyCtYneqq7WMa3-tLCZwlEhh4Mrn6GtHTpg",
+    "authDomain": "pythonserver-8898f.firebaseapp.com",
+    "databaseURL": "https://pythonserver-8898f.firebaseio.com",
+    "projectId": "pythonserver-8898f",
+    "storageBucket": "pythonserver-8898f.appspot.com",
+    "messagingSenderId": "101060210218"
+}
+firebase = pyrebase.initialize_app(config)
 
-# Create an instance of the sensehat
-sense = SenseHat()
-    
-# Utilities
-hex2rgb = lambda hx: (int(hx[1:3],16),int(hx[3:5],16),int(hx[5:7],16))
+try:
+    #connect to firebase
+    db = firebase.database()
 
-# Define the api_environment route
-@app.route('/', methods=['GET', 'POST'])
-def page():
-    return "Data is being sent."
+    # Create an instance of flask
+    app = Flask(__name__)
 
-# Connect to firebase
-cred = credentials.Certificate('./credential.json')
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://pythonserver-8898f.firebaseio.com/'
-})
+    # Create an instance of the sensehat
+    sense = SenseHat()
+        
+    # Utilities
+    hex2rgb = lambda hx: (int(hx[1:3],16),int(hx[3:5],16),int(hx[5:7],16))
 
-ref = db.reference('/ambilight/')
+    # Define the route
+    @app.route('/', methods=['GET', 'POST'])
+    def page():
+        return "Data is being sent."
 
+    def setLight():
+        if (db.child('ambilight').get().val()):
+            hexColor = db.child('ambilight').get().val()
+            rgbColor = hex2rgb(hexColor)
+            sense.clear(rgbColor)
 
-def setLight():
-    color = (hex2rgb(hex2rgb(ref.get()["color"])))
-    sense.clear(color)
+    while(1):
+        setLight()
 
-while(1):
-    setLight()
+    # Main method for Flask server
+    if __name__ == '__main__':
+        app.run(host = '192.168.5.198', port = 8081, debug = True)
 
-# Main method for Flask server
-if __name__ == '__main__':
-  app.run(host = '192.168.5.198', port = 8081, debug = True)
+except (KeyboardInterrupt, SystemExit):
+	sense.clear()
+	print('\n' + 'Stopped ColorPicker')
+	sys.exit(0)
